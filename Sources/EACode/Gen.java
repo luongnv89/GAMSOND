4cbb5b7fc780084f663c037ac034dddb06167a6f
luongnv89@gmail.com <luongnv89@gmail.com@af8a745f-2e23-e863-8dbc-8667c16aba0d>
2012-04-09 13:38:43 +0000

    - Các bài báo cáo

---
+++ Sources/EACode/Gen.java
@@ -0,0 +1,25 @@
+package System;
+
+/**
+ * @version 1.0
+ * @created 07-Apr-2012 4:50:55 PM
+ */
+public class Gen {
+
+	private int id;
+	private Edge listEdge;
+	private int requestID;
+
+	public Gen(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public Gen initGen(){
+		return null;
+	}
+
+}
---
+++ Sources/EACode/Individual.java
@@ -0,0 +1,31 @@
+package System;
+
+/**
+ * @author crocode
+ * @version 1.0
+ * @created 07-Apr-2012 4:50:45 PM
+ */
+public class Individual {
+
+	private Gen bPaths;
+	private float fitness;
+	private int id;
+	private Gen wPaths;
+
+	public Individual(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public float getFitness(){
+		return null;
+	}
+
+	public Individual initIndividual(){
+		return null;
+	}
+
+}
